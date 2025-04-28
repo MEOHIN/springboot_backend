a9a0cd1dc2164c657338ca1219d1d86a8925a8f8
meohin <meohin.meo@gmail.com>
2025-04-28 17:51:19 +0900

    feat: Post 엔티티 생성

---
+++ src/main/java/com/meohin/springboot_backend/model/Post.java
@@ -0,0 +1,29 @@
+package com.meohin.springboot_backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor  // 기본 생성자를 자동으로 생성
+@AllArgsConstructor // 모든 필드를 인자로 받는 생성자를 자동으로 생성
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 5000)
+    private String content;
+}
