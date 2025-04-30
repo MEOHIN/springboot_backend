6a2cd5808845b75d7a4cdfcbc2b1a667e9a3be77
meohin <meohin.meo@gmail.com>
2025-04-30 17:47:44 +0900

    feat: 게시글 수정 API 추가
    - 게시글 정보를 수정할 수 있는 PUT /api/posts/{id} API 추가
    - 요청 본문으로 수정된 게시글 정보를 받아 기존 게시글을 업데이트
    - 수정된 게시글을 응답으로 반환

--- src/main/java/com/meohin/springboot_backend/controller/PostController.java
+++ src/main/java/com/meohin/springboot_backend/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,5 +48,13 @@ public class PostController {
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
         Post createdPost = postService.createPost(post);
         return ResponseEntity.ok(createdPost);
+    }
+
+    // 게시글 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post updatedPost) {
+        Optional<Post> updated = postService.updatePost(id, updatedPost);
+        return updated.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
